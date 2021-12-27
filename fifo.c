8d0ad8a612c9fb21b3529e7a89e1c1ac38335456
djokovic55 <djokovic64@gmail.com>
2021-12-27 10:33:33 +0100

    Issue with semaphore

--- fifo.c
+++ fifo.c
@@ -9,6 +9,9 @@
 #include <linux/uaccess.h>
 #include <linux/errno.h>
 #include <linux/device.h>
+#include <linux/wait.h>
+#include <linux/semaphore.h>
+
 #define BUFF_SIZE 20
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -17,8 +20,13 @@ static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
 
+DECLARE_WAIT_QUEUE_HEAD(readQ);
+DECLARE_WAIT_QUEUE_HEAD(writeQ);
+struct semaphore sem;
+
+
 int flag = 0;
-int fifo[16];
+long long fifo[16];
 int pr = 0;
 int pw = 0 ;
 int endRead = 0;
@@ -61,10 +69,21 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 
 	if(!flag)
 	{
-
+			
+		if(down_interruptible(&sem))
+			return -ERESTARTSYS;
+		while(pr == 0)
+		{
+			up(&sem);
+			if(wait_event_interruptible(writeQ,(pr < pw)))
+				return -ERESTARTSYS;
+			if(down_interruptible(&sem))
+				return -ERESTARTSYS;
+		}
 		if(pr < pw)
 		{
-			len = scnprintf(buff, BUFF_SIZE, "%d ", fifo[pos]);
+				
+			len = scnprintf(buff, BUFF_SIZE, "%lld ", fifo[pr]);
 			ret = copy_to_user(buffer, buff, len);
 			if(ret)
 				return -EFAULT;
@@ -77,7 +96,10 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 		{
 				printk(KERN_WARNING "Fifo is empty\n");
 		}
+		up(&sem);
+		wake_up_interruptible(&writeQ);
 	}
+	
 	else 
 	{
 		
@@ -87,7 +109,7 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 				flag = 1;
 			}
 	
-			len = scnprintf(buff, BUFF_SIZE, "%d ", fifo[pos]);
+			len = scnprintf(buff, BUFF_SIZE, "%lld ", fifo[pr]);
 			ret = copy_to_user(buffer, buff, len);
 			if(ret)
 				return -EFAULT;
@@ -101,7 +123,7 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
 {
         char buff[BUFF_SIZE];
-        int value;
+        long long value;
         int ret;
 
         ret = copy_from_user(buff, buffer, length);
@@ -111,13 +133,24 @@ ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length,
 
         if(pw < pr)
         {
+				if(down_interruptible(&sem))
+					return -ERESTARTSYS;
+				while(pw == 0)
+				{
+					up(&sem);
+					if(wait_event_interruptible(writeQ,(flag)))
+						return -ERESTARTSYS;
+					if(down_interruptible(&sem))
+						return -ERESTARTSYS;
+				}
+		
 				if(flag)
 				{
 					//UPIS
-					ret = sscanf(buff,"%d",&value);
+					ret = sscanf(buff,"0b%lld;",&value);
 					if(ret==1)//one parameter parsed in sscanf
 					{
-						printk(KERN_INFO "Succesfully wrote value %d", value);
+						printk(KERN_INFO "Succesfully wrote value  0b%lld", value);
 						fifo[pw] = value;
 					}
 					else
@@ -134,16 +167,30 @@ ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length,
 				}
 				else
 					printk(KERN_WARNING "Fifo is full\n");
-		
+
+				up(&sem);
+				wake_up_interruptible(&readQ);
+		}		
         else
         {
+				
+				if(down_interruptible(&sem))
+					return -ERESTARTSYS;
+				while(pw == 0)
+				{
+					up(&sem);
+					if(wait_event_interruptible(writeQ,(!flag)))
+						return -ERESTARTSYS;
+					if(down_interruptible(&sem))
+						return -ERESTARTSYS;
+				}
 				if(!flag)
 				{
 					//UPIS
-					ret = sscanf(buff,"%d",&value);
+					ret = sscanf(buff,"0b%lld;",&value);
 					if(ret==1)//one parameter parsed in sscanf
 					{
-						printk(KERN_INFO "Succesfully wrote value %d", value);
+						printk(KERN_INFO "Succesfully wrote value  0b%lld", value);
 						fifo[pw] = value;
 					}
 					else
@@ -161,6 +208,10 @@ ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length,
 				}
 				else 
 					printk(KERN_WARNING "Fifo is full\n");
+
+
+				up(&sem);
+				wake_up_interruptible(&readQ);
 		}
 
         return length;
@@ -169,6 +220,8 @@ static int __init fifo_init(void)
 {
    int ret = 0;
         int i=0;
+
+		sema_init(&sem,1);
 
         //Initialize array
         for (i=0; i<10; i++)
